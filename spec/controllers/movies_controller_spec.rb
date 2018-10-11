a3b5ae0016d46e7239ce9fd4ed039d20ea898e2b
Xinyu Gong <xy_Gong@foxmail.com>
2018-10-10 19:12:26 -0500

    fix syntax error

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -15,71 +15,71 @@ RSpec.describe MoviesController, type: :controller do
     end
     
     describe "POST #create" do
-        it "creating a new movie in the record" do
+        it "should create a new movie in the record" do
             expect {
             post :create, movie: @mock_movie_attributes
             }.to change(Movie,:count).by(1)
         end
-        it "assigning the saved movie to @movie" do
+        it "should assign the saved movie to @movie" do
             post :create, movie: @mock_movie_attributes
             expect(assigns(:movie).title).to include("Dunkirk")
         end
-        it "redirecting to the homepage" do
+        it "should redirect to the homepage" do
             post :create, movie: @mock_movie_attributes
             expect(response).to redirect_to(:action => 'index')
         end
     end
     
     describe "GET #show" do
-      it "passing the requested movie to @movie" do
+      it "should pass the requested movie to @movie" do
         get :show, id: @mock_movie
         expect(assigns(:movie).title).to include("Inception")
       end
-      it "rendering the show template" do
+      it "should render the show template" do
         get :show, id: @mock_movie
         expect(response).to render_template(:show)
       end
     end
     
     describe "PUT #update" do
-      it "locating the requested movie" do
+      it "should locate the requested movie" do
         put :update, id: @mock_movie, movie: @mock_movie_attributes
         expect(assigns(:movie)).to eq(@mock_movie)
       end
-      it "changing the requested movies attributes" do
+      it "should change the requested movies attributes" do
         put :update, id: @mock_movie, movie: @mock_movie_attributes
         @mock_movie.reload
         expect(@mock_movie.title).to eq("Dunkirk")
         expect(@mock_movie.rating).to eq("PG")
       end
-      it "redirecting to the updated movie" do
+      it "should redirect to the updated movie" do
         put :update, id: @mock_movie, movie: @mock_movie_attributes
         expect(response).to redirect_to @contact
       end
     end
     
     describe "DELETE #destroy" do
-      it "deleteing the movie" do
+      it "should delete the movie" do
         expect{
           delete :destroy, id: @mock_movie
         }.to change(Movie,:count).by(-1)
       end
-      it "redirecting to the main page " do
+      it "should redirect to the main page " do
         delete :destroy, id: @mock_movie
         expect(response).to redirect_to(:action => 'index') 
       end
     end
     
   describe 'GET index' do
-    it 'rendering the index template' do
+    it 'should render the index template' do
       get :index
       expect(response).to render_template('index')
     end
-    it 'assigning instance variable for title header' do
+    it 'should assign instance variable for title header' do
       get :index, {sort: 'title'}
       expect(assigns(:title_header)).to eql('hilite')
     end
-    it 'assigning instance variable for release_date header' do
+    it 'should assign instance variable for release_date header' do
       get :index, { sort: 'release_date'}
       expect(assigns(:date_header)).to eql('hilite')
     end
